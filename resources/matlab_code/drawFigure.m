e7dd3cf65e65ae8df6a719887e925f7845fd54db
Tom Lau <active52004@gmail.com>
2014-05-15 11:08:44 +1000

    approach draft updated

--- resources/matlab_code/drawFigure.m
+++ resources/matlab_code/drawFigure.m
@@ -17,52 +17,66 @@ M1=csvread(file1);
 M2=csvread(file2);
 M3=csvread(file3);
 
-M_live = M3;
+M_live = M1;
+least = [];
 
 %find all pattern value in column 1 as x, store in C
+
 A = [0,17,34,51,68, 85, 102, 119, 136, 153, 170, 187,204, 221, 238, 255];
 X = 0:255;
-%B = A;
 %Both no-pattern
 B = setdiff(X,A);
 D = B;
 C = [];
 for i=D
     for j=B
-       C = [C i*256+j+1]; 
+       C = [C i*256+j+1];
     end
 end
 M_tmp = M_live(C,2)/MaxValue;
-scatter(C,M_tmp,10,'o','b');
+scatter(C,M_tmp,20,'o','b');
 %Both pattern
 hold on
 C=[];
 B = A;
 for i=A
     for j=B
-       C = [C i*256+j+1]; 
+       C = [C i*256+j+1];
     end
 end
 M_tmp = M_live(C,2)/MaxValue;
-scatter(C,M_tmp,5,'fill','r');
+scatter(C,M_tmp,20,'fill','r');
 %pattern and no-pattern
 hold on
 C=[];
 B = setdiff(X,A);
 for i=A
     for j=B
-       C = [C i*256+j+1]; 
+       C = [C i*256+j+1];
     end
 end
 
 for i=B
     for j=A
-       C = [C i*256+j+1]; 
+       C = [C i*256+j+1];
     end
 end
+
+for i=X
+    for j=X
+       k = i*256+j+1;
+       if M_live(k,2)/MaxValue >0 && M_live(k,2)/MaxValue < 0.1
+           least = [least k-1];
+       end
+    end
+end
+least = least';
+str = dec2bin(least,16);
+tmp_int = M_live(21845,2)
 M_tmp = M_live(C,2)/MaxValue;
-scatter(C,M_tmp,5,'v','g');
-%use C as x M(C,2) as y to draw the bar
+scatter(C,M_tmp,20,'v','g');
+
+
 legend('Both no-pattern','Both pattern', 'Pattern and no-pattern')
 
 
