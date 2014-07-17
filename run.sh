489ee9d5c1af2a0d637a6dcb86fe1d00b0880f66
Tom Lau <active52004@gmail.com>
2014-07-17 12:03:23 +1000

    approach draft version 17/07/2014

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ texFile="approach_draft"
 
 pdflatex ${texFile}.tex 
 makeindex ${texFile}.nlo -s nomencl.ist -o ${texFile}.nls
+bibtex ${texFile}.aux
+pdflatex ${texFile}.tex
+pdflatex ${texFile}.tex
 pdflatex ${texFile}.tex
